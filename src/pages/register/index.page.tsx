c3b2c91c94618acf8cc8d237d1759ad0cd8e818b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-06-29 20:46:48 -0300

    feat: Calendar Connection Page

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -41,6 +41,8 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (err) {
       if (err instanceof AxiosError && err.response?.data?.message) {
         const { message } = err.response?.data
