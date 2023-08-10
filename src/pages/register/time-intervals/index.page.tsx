c0c83e0f870fa7139ffa954ae33765365866d364
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-08-10 20:16:22 -0300

    feat: Availability page

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,60 @@
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import {
+  IntervalBox,
+  IntervalContainer,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+} from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil!, Ah, você
+          pode editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+      <IntervalBox as="form">
+        <IntervalContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Segunda-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="md" type="time" step={60} />
+              <TextInput size="md" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Terça-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="md" type="time" step={60} />
+              <TextInput size="md" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+        </IntervalContainer>
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  )
+}
