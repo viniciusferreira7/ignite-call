9c7f9f36a1e191dfb0f4d0e7fe73f0ec967a926a
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-09-04 19:25:33 -0300

    style: ESLint configuration

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -5,12 +5,12 @@ export const globalStyles = globalCss({
     boxSizing: 'border-box',
     padding: 0,
     margin: 0,
-    fontFamily: '$default'
+    fontFamily: '$default',
   },
 
   body: {
     backgroundColor: '$gray900',
     color: '$gray100',
-    '-webkit-font-smoothing': 'antialiased'
-  }
+    '-webkit-font-smoothing': 'antialiased',
+  },
 })
