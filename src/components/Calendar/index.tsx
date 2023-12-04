1d3721d1efd706f16dc671654dc9b819f9d1ee44
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-12-04 11:08:27 -0300

    feat: Calendar styling

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -3,6 +3,7 @@ import {
   CalendarActions,
   CalendarBody,
   CalendarContainer,
+  CalendarDay,
   CalendarHeader,
   CalendarTitle,
 } from './styles'
@@ -36,13 +37,27 @@ export function Calendar() {
         </thead>
         <tbody>
           <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>4</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>5</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>6</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>7</CalendarDay>
+            </td>
           </tr>
         </tbody>
       </CalendarBody>
