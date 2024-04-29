2f60ecaaa62aecf8545ab01d2e755fd8ebb151f2
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-04-29 11:24:44 -0300

    feat: error message and settings

--- src/app/register/connect-calendar/components/connect-form.tsx
+++ src/app/register/connect-calendar/components/connect-form.tsx
@@ -1,39 +1,57 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { IconArrowRight } from '@tabler/icons-react'
+import { IconArrowRight, IconCheck } from '@tabler/icons-react'
 import { signIn, useSession } from 'next-auth/react'
+import { useSearchParams } from 'next/navigation'
 
 export function ConnectForm() {
+  const searchParams = useSearchParams()
   const session = useSession()
 
   const isAuthenticated = session.status === 'authenticated'
+
+  const hasAuthError = searchParams.get('error') === 'permissions'
 
   return (
     <form className="mt-6 space-y-4 rounded-md bg-gray-800 p-4 md:p-6 ">
       <div className="flex flex-col items-center justify-between gap-y-2 rounded-md border border-gray-600 px-5 py-[1.375rem] md:flex-row">
         <p className="text-base font-medium text-gray-100">Google Calendar</p>
-        <Button
-          variant="outline"
-          type="button"
-          disabled={isAuthenticated}
-          onClick={() =>
-            signIn('google', { callbackUrl: '/register/connect-calendar' })
-          }
-        >
-          {isAuthenticated ? (
-            'Conectado'
-          ) : (
-            <>
-              Conectar <IconArrowRight className="size-4" />
-            </>
-          )}
-        </Button>
+        {isAuthenticated ? (
+          <Button
+            variant="outline"
+            type="button"
+            disabled={true}
+            className="bg-green-600 text-white"
+            onClick={() =>
+              signIn('google', { callbackUrl: '/register/connect-calendar' })
+            }
+          >
+            Conectado <IconCheck stroke={1.5} className="size-5" />
+          </Button>
+        ) : (
+          <Button
+            variant="outline"
+            type="button"
+            onClick={() =>
+              signIn('google', { callbackUrl: '/register/connect-calendar' })
+            }
+          >
+            Conectar <IconArrowRight className="size-5" />
+          </Button>
+        )}
       </div>
+      {hasAuthError && (
+        <p className="text-xs font-medium text-red-500">
+          Falha ao se conectar ao Google, verifique se você habilitou as
+          permissões de acesso ao Google Calendar.
+        </p>
+      )}
       <Button
         variant="secondary"
         size="h-auto"
         className="flex w-full items-center py-3"
+        disabled={!isAuthenticated}
       >
         Próximo passo <IconArrowRight size={16} />
       </Button>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap duration-300 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center gap-2 justify-center whitespace-nowrap duration-300 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
